c1fb88fc49252bee693739a43fc0f378d2dd1be6
Venkata Rakesh Nallam <vnallam@hawk.iit.edu>
2018-07-29 14:02:47 -0700

    fix() : modifying query

--- controller/jobsController.js
+++ controller/jobsController.js
@@ -1,42 +1,37 @@
 'use strict'
-var mongoose = require('mongoose'),
+var mongoose = require('mongoose');
+var mongodb = require("mongodb");
+var BSONRegExp = mongodb.BSONRegExp,
 Jobs = mongoose.model('Jobs');
+//db.users.find(name: new RegExp(search， ‘i')) //For substring search, case insensitive
+
 
 exports.list_all_jobs = function(req,res){
 	 var payRate = undefined!==req.query["payRate"]&&req.query["payRate"]!=""? req.query["payRate"].split(","): "";
-	 console.log(payRate);
+	 //console.log(req.query["availability"]);
+	 var availability = req.query["availability"]!=""?req.query["availability"].split(",") :""
+	 console.log(availability);
+	 //var regexKeywords = new BSONRegExp(\\.\\*req.query["keywords"]\\.\\*","i");
+	 
 	 var query = {
         "$or": [
-            {
-                "rolename": req.query["keywords"]
-            },
-            {
+			{
                 "skills": {
                     "$in": [
                         req.query["skills"]
                     ]
                 }
             },
-            {
+			{
                 "type": {
-                    "$in": [
-                        req.query["jobType"]
-                    ]
+                    "$in": availability
                 }
             },
             {
                 "rate": {
-                    "$gte": payRate[0]
+                    "$gte": payRate[0],
+					"$lte": payRate[1]
                 }
-            },
-            {
-                "$and": [
-                    {
-                        "rate": {
-                            "$lte": payRate[1]
-                        }
-                    }
-                ]
             },
             {
                 "experiencelevel": req.query["experienceLevel"]
@@ -52,7 +47,12 @@ exports.list_all_jobs = function(req,res){
                 }
             }
         ]
-    };
+    }
+	
+	 if(req.query["keywords"]!==""){
+		 query.rolename = new RegExp(req.query["keywords"],'i');
+	 }	
+
 	Jobs.find(query,function(err,job){
 		if(err)
 			res.send(err);
