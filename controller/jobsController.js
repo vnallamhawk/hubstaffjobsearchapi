0664656b146a9321a1653f17e3a7cef26326b330
Venkata Rakesh Nallam <vnallam@hawk.iit.edu>
2018-08-02 23:43:33 -0700

    fix():updating query

--- controller/jobsController.js
+++ controller/jobsController.js
@@ -8,20 +8,21 @@ Jobs = mongoose.model('Jobs');
 
 exports.list_all_jobs = function(req,res){
 	 var payRate = undefined!==req.query["payRate"]&&req.query["payRate"]!=""? req.query["payRate"].split(","): "";
+	 console.log(payRate);
 	 //console.log(req.query["availability"]);
-	 var availability = req.query["availability"]!=""?req.query["availability"].split(",") :""
+	 var availability = undefined!==req.query["availability"]&& req.query["availability"]!=""?req.query["availability"].split(",") :""
 	 console.log(availability);
 	 //var regexKeywords = new BSONRegExp(\\.\\*req.query["keywords"]\\.\\*","i");
 	 
 	 var query = {
         "$or": [
-			{
-                "skills": {
-                    "$in": [
-                        req.query["skills"]
-                    ]
-                }
-            },
+		{
+			"rolename" : new RegExp(req.query["keywords"],'i')
+		},
+		{
+			"description" : new RegExp(req.query["keywords"],'i')
+		},
+		
 			{
                 "type": {
                     "$in": availability
@@ -49,6 +50,61 @@ exports.list_all_jobs = function(req,res){
         ]
     }
 	
+	// "$and" : [
+           // {
+                // "skills": {
+                    // "$in": [
+                        // req.query["skills"]
+                    // ]
+                // }
+            // },
+        // ]
+	 // filter the search by any criteria given by the user
+		query['$and']=[];
+		if(req.query.skills!==""){ 
+		//query['$and']=[];
+		console.log("inside1");// if the criteria has a value or values
+		query["$and"].push({ skills: {$in: req.query["skills"]}}); // add to the query object
+		}
+		if(req.query.availability!==""){ 
+		//query['$and']=[];
+		console.log("inside2");// if the criteria has a value or values
+		query["$and"].push({ type: {$in: req.query["availability"]}}); // add to the query object
+		}
+		if(req.query.jobType!==""){ 
+		//query['$and']=[];
+		//console.log("inside2");// if the criteria has a value or values
+		query["$and"].push({ type: {$in: req.query["jobType"]}}); // add to the query object
+		}
+		if(req.query.payRate!==""){ 
+		query["$and"].push({ rate: {
+                    "$gte": payRate[0],
+					"$lte": payRate[1]
+                }}); // add to the query object
+		}
+		if(req.query.experienceLevel!==""){ 
+		query["$and"].push({
+                "experiencelevel": req.query["experienceLevel"]
+            }); // add to the query object
+		}
+		if(req.query.country!==""){ 
+		query["$and"].push({
+                "country": req.query["country"] 
+            }); // add to the query object
+		}
+		if(req.query.languages!==""){ 
+		query["$and"].push({
+                "languages": {
+                    "$in": [
+                        req.query["languages"]
+                    ]
+                }
+            }); // add to the query object
+		}
+		
+		
+
+	
 	 if(req.query["keywords"]!==""){
 		 query.rolename = new RegExp(req.query["keywords"],'i');
 	 }	
@@ -59,3 +115,4 @@ exports.list_all_jobs = function(req,res){
 		res.json(job);
 	});	
 }
+
